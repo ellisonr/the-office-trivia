6414ac5a3c83b68651ae8b6c882719a835ef9b6d
Ellison Rio <rio.ellison@gmail.com>
2019-05-24 01:33:36 -0400

    third question added; changed divs to spans so that they're horizontal

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //function for checking and keeping score
 function scoring(){
     var q1 = document.questions.character.value
+    var q2 = docment.questions.character.value
+    var q3 = document.questions.character.value
     var correctAnswers = 0
 //if user pics the following answer for the following question, add to their correctAnswers score
 if (q1 === "dwight"){correctAnswers ++}
 if (q2 === "ryan"){correctAnswers ++}
+if (q3 === "creed"){correctAnswers ++}
 }
