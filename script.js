dbeb081fac548aa3c090ff575d3c480bf3a7a4a6
Ellison Rio <rio.ellison@gmail.com>
2019-05-24 02:34:50 -0400

    finished the rest of the questions and updated corresponding css and javascript

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ function scoring(){
     var q1 = document.questions.character.value
     var q2 = docment.questions.character.value
     var q3 = document.questions.character.value
+    var q4 = document.questions.character.value
+    var q5 = document.questions.character.value
+    var q6 = document.questions.character.value
+    var q7 = document.questions.character.value
+    var q8 = document.questions.character.value
+    var q9 = document.questions.character.value
+    var q10 = document.questions.character.value
     var correctAnswers = 0
 //if user pics the following answer for the following question, add to their correctAnswers score
 if (q1 === "dwight"){correctAnswers ++}
 if (q2 === "ryan"){correctAnswers ++}
 if (q3 === "creed"){correctAnswers ++}
+if (q4 === "andy"){correctAnswers ++}
+if (q5 === "michael"){correctAnswers ++}
+if (q6 === "kelly"){correctAnswers ++}
+if (q7 === "darryl"){correctAnswers ++}
+if (q8 === "deangelo"){correctAnswers ++}
+if (q9 === "toby"){correctAnswers ++}
+if (q10 === "gabe"){correctAnswers ++}
 }
