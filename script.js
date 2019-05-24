592c4ce95a3df55fc1fd89bfbe8e3317286f100a
Ellison Rio <rio.ellison@gmail.com>
2019-05-24 01:20:08 -0400

    added second question in html and updated javascript

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function scoring(){
     var q1 = document.questions.character.value
     var correctAnswers = 0
 //if user pics the following answer for the following question, add to their correctAnswers score
-if (q1 === "dwight"){correctAnswer ++}
+if (q1 === "dwight"){correctAnswers ++}
+if (q2 === "ryan"){correctAnswers ++}
+}
