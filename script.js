3a6e352ad2b94fdee56ad989f0a7c972050f8c1c
Ellison Rio <rio.ellison@gmail.com>
2019-05-24 10:10:53 -0400

    made a lot of the JS DRY, but saved the old way in case i don't get the DRY way to work by 1:30

--- script.js
+++ script.js
@@ -26,10 +26,34 @@ document.getElementById("final-score").style.visibility="visible"
 document.getElementById("score").innerHTML = "You got "+correctAnswers+" out of 10!"
 alert(" ' 'You miss 100% of the shots you don't take.'\n            -Wayne Gretzky'\n                        -Michael Scott")
 }
+//----------------------------------------
+//function for checking and keeping score
+// function scoring(){
 
-// const getScore = document.querySelector("#submit")
-// getScore.addEventListener("click",function(e){
-//     e.preventDefault()
-//     document.getElementById("final-score").style.visibilty = "visible";
+//     //if user pics the following answer for the following question, add to their correctAnswers score
+//     answers = ["dwight","ryan","creed","andy","michael","erin","darryl","deangelo","toby","gabe"];
+//     console.log(answers);
+    
+//     for(let i =0;i<answers.length; i++){
+//         var q = document.questions.q+[i].value
+//         var correctAnswers = 0
+//         if (q+[i] === answers[i]){
+//             correctAnswers ++;
+//         }
+//     }
+//     console.log(correctAnswers);
+//     // if (q1 === "dwight"){correctAnswers ++}
+//     // if (q2 === "ryan"){correctAnswers ++}
+//     // if (q3 === "creed"){correctAnswers ++}
+//     // if (q4 === "andy"){correctAnswers ++}
+//     // if (q5 === "michael"){correctAnswers ++}
+//     // if (q6 === "erin"){correctAnswers ++}
+//     // if (q7 === "darryl"){correctAnswers ++}
+//     // if (q8 === "deangelo"){correctAnswers ++}
+//     // if (q9 === "toby"){correctAnswers ++}
+//     // if (q10 === "gabe"){correctAnswers ++}
+    
+//     document.getElementById("final-score").style.visibility="visible"
 //     document.getElementById("score").innerHTML = "You got "+correctAnswers+" out of 10!"
-// })
+//     alert(" ' 'You miss 100% of the shots you don't take.'\n            -Wayne Gretzky'\n                        -Michael Scott")
+//     }
