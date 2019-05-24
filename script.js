aeac76a69d08d914f7af29cd3e8b81ef9f378609
Ellison Rio <rio.ellison@gmail.com>
2019-05-24 03:57:51 -0400

    added alert and fixed some format, got scoring to work

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //function for checking and keeping score
 function scoring(){
-    var q1 = document.questions.character.value
-    var q2 = docment.questions.character.value
-    var q3 = document.questions.character.value
-    var q4 = document.questions.character.value
-    var q5 = document.questions.character.value
-    var q6 = document.questions.character.value
-    var q7 = document.questions.character.value
-    var q8 = document.questions.character.value
-    var q9 = document.questions.character.value
-    var q10 = document.questions.character.value
+    var q1 = document.questions.q1.value
+    var q2 = document.questions.q2.value
+    var q3 = document.questions.q3.value
+    var q4 = document.questions.q4.value
+    var q5 = document.questions.q5.value
+    var q6 = document.questions.q6.value
+    var q7 = document.questions.q7.value
+    var q8 = document.questions.q8.value
+    var q9 = document.questions.q9.value
+    var q10 = document.questions.q10.value
     var correctAnswers = 0
 //if user pics the following answer for the following question, add to their correctAnswers score
 if (q1 === "dwight"){correctAnswers ++}
@@ -17,12 +17,19 @@ if (q2 === "ryan"){correctAnswers ++}
 if (q3 === "creed"){correctAnswers ++}
 if (q4 === "andy"){correctAnswers ++}
 if (q5 === "michael"){correctAnswers ++}
-if (q6 === "kelly"){correctAnswers ++}
+if (q6 === "erin"){correctAnswers ++}
 if (q7 === "darryl"){correctAnswers ++}
 if (q8 === "deangelo"){correctAnswers ++}
 if (q9 === "toby"){correctAnswers ++}
 if (q10 === "gabe"){correctAnswers ++}
-
-document.getElementById("final-score").style.visibilty = "visible";
+document.getElementById("final-score").style.visibility="visible"
 document.getElementById("score").innerHTML = "You got "+correctAnswers+" out of 10!"
+alert(" ' 'You miss 100% of the shots you don't take.'\n            -Wayne Gretzky'\n                        -Michael Scott")
 }
+
+// const getScore = document.querySelector("#submit")
+// getScore.addEventListener("click",function(e){
+//     e.preventDefault()
+//     document.getElementById("final-score").style.visibilty = "visible";
+//     document.getElementById("score").innerHTML = "You got "+correctAnswers+" out of 10!"
+// })
