cca0ec02dea73f9cc58e04aff2e521ba793a813a
Ellison Rio <rio.ellison@gmail.com>
2019-05-24 00:44:50 -0400

    added first sample question in html, added css for it, and first pass on javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+//function for checking and keeping score
+function scoring(){
+    var q1 = document.questions.character.value
+    var correctAnswers = 0
+//if user pics the following answer for the following question, add to their correctAnswers score
+if (q1 === "dwight"){correctAnswer ++}
